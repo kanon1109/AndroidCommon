b460ffb305cacc2c729a6abc44fbd62dee6d8bee
kanontang <kanontang@gmail.com>
2016-05-08 15:31:57 +0800

    增加unity的安卓常用系统功能扩展

---
+++ Assets/Scripts/AndroidCommon.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndroidCommon : MonoBehaviour 
+{
+    //安卓类
+#if UNITY_ANDROID
+    private static AndroidJavaClass ajc = null;
+#endif
+
+    /// <summary>
+    /// 显示原生tips
+    /// </summary>
+    /// <param name="content">内容</param>
+    public static void showTips(string content)
+    {
+#if UNITY_ANDROID
+        if (ajc == null)
+            ajc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject ajo = ajc.GetStatic<AndroidJavaObject>("currentActivity");
+        ajo.Call("showTips", new string[] { content });
+#endif
+    }
+
+    /// <summary>
+    /// 显示对话框
+    /// </summary>
+    /// <param name="title">标题</param>
+    /// <param name="content">内容</param>
+    /// <param name="buttonLable">按钮标签</param>
+    public static void showDialog(string title, string content, string buttonLable)
+    {
+#if UNITY_ANDROID
+        if (ajc == null)
+            ajc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject ajo = ajc.GetStatic<AndroidJavaObject>("currentActivity");
+        ajo.Call("showDialog", new string[] { title, content, buttonLable });
+#endif
+    }
+    
+    /// <summary>
+    /// 调用unity的方法
+    /// </summary>
+    /// <param name="unityScriptName">unity脚本名称</param>
+    /// <param name="unityFunctionName">unity方法名</param>
+    /// <param name="arg">unity参数</param>
+    public static void invokeUnity(string unityScriptName, string unityFunctionName, string arg)
+    {
+#if UNITY_ANDROID
+        if (ajc == null)
+            ajc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject ajo = ajc.GetStatic<AndroidJavaObject>("currentActivity");
+        ajo.Call("invokeUnity", new string[] { unityScriptName, unityFunctionName, arg });
+#endif
+    }
+}
---
+++ Assets/Scripts/AndroidCommonTest.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AndroidCommonTest : MonoBehaviour
+{
+    public Button showTipsBtn;
+    public Button showDialogBtn;
+    public Button setVibratorBtn;
+    public Button invokeUnityBtn;
+    public Text logTxt;
+    // Use this for initialization
+    void Start()
+    {
+        this.showTipsBtn.onClick.AddListener(showTipsBtnClickHandler);
+        this.showDialogBtn.onClick.AddListener(showDialogBtnClickHandler);
+        this.invokeUnityBtn.onClick.AddListener(invokeUnityBtnClickHandler);
+    }
+    
+    private void unityFunction(string arg)
+    {
+        this.logTxt.text = "unityFunction";
+        AndroidCommon.showTips("调用unityFunction 参数是：" + arg);
+    }
+
+    private void invokeUnityBtnClickHandler()
+    {
+        this.logTxt.text = "invokeUnityBtnClickHandler";
+        AndroidCommon.invokeUnity(this.name, "unityFunction", "aaa");
+    }
+
+    private void showDialogBtnClickHandler()
+    {
+        this.logTxt.text = "showDialogBtnClickHandler";
+        AndroidCommon.showDialog("对话框", "这是对话框", "确定");
+    }
+
+    private void showTipsBtnClickHandler()
+    {
+        this.logTxt.text = "showTipsBtnClickHandler";
+        AndroidCommon.showTips("这是tips");
+    }
+}
